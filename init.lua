ca381bde5f0b77a5a3504263a40367674d22da61
Joseph Tongay <dirk.tongay@gmail.com>
2024-01-04 10:33:37 -0600

    splitting out into sep files

--- init.lua
+++ init.lua
@@ -70,11 +70,6 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Auto Pair quotes/parens/brackets/etc.
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-  },
   -- {
   --   'vuki656/package-info.nvim',
   --   dependencies = {
@@ -83,33 +78,14 @@ require('lazy').setup({
   -- },
 
   -- VimBeGood, practing and learning vim control
-  'ThePrimeagen/vim-be-good',
 
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-  },
 
   {
     -- Autocompletion
---
+++ lua/custom/plugins/lsp-config.lua
@@ -0,0 +1,88 @@
+--return {
+  -- LSP Configuration & Plugins
+  --'neovim/nvim-lspconfig',
+  --dependencies = {
+    -- Automatically install LSPs to stdpath for neovim
+    --{ 'williamboman/mason.nvim', opts = {} },
+    --{
+      --'williamboman/mason-lspconfig.nvim',
+      --opts = {
+        --ensure_installed = {
+          --"lua_ls",
+          -- "graphql",
+          -- "html",
+          -- "htmx",
+          -- "tsserver",
+          -- "ruby_ls",
+          --"gopls"
+        --}
+      --}
+      -- config = function ()
+      --   require('mason-lspconfig').setup({
+
+      -- })
+      -- end
+    --},
+
+    -- Useful status updates for LSP
+    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+    -- { 'j-hui/fidget.nvim',       opts = {} },
+
+    -- Additional lua configuration, makes nvim stuff amazing!
+    --'folke/neodev.nvim',
+  --}
+--}
+
+return {
+  {
+    'williamboman/mason.nvim',
+    config = function ()
+      require("mason").setup()
+    end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function ()
+      require('mason-lspconfig').setup({
+        ensure_installed = {'tsserver', 'lua_ls','graphql', 'html', 'htmx', 'gopls' }
+      })
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      {'j-hui/fidget.nvim', opts = {}},
+      'fole/neodev.nvim'
+    },
+    config = function ()
+      vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>f', function()
+          vim.lsp.buf.format { async = true }
+        end, opts)
+      end,
+    })
+    end
+  }
+}
